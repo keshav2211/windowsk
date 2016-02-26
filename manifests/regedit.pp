091d6e5f9d3198fee951e1fa67811269528a77c0
Keshav Sharma <keshav2211@gmail.com>
2016-02-26 10:32:53 +0100

    key changed

--- manifests/regedit.pp
+++ manifests/regedit.pp
@@ -2,6 +2,7 @@ class windowsk::regedit {
 
 registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
   ensure => present,
+  type   => dword,
   data   => '00000000',
 }
 }
