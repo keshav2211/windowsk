6e42e26184f916465b4ca62c1ad0424c577c62cb
Keshav Sharma <keshav2211@gmail.com>
2016-02-26 10:34:34 +0100

    key changed

--- manifests/regedit.pp
+++ manifests/regedit.pp
@@ -5,4 +5,11 @@ registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed C
   type   => dword,
   data   => '00000000',
 }
+
+registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
+  ensure => present,
+  type   => dword,
+  data   => '00000000',
+}
+
 }
