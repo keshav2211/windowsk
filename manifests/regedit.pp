7890731577df8acb9b27677e7d296da1261c2b7d
Keshav Sharma <keshav2211@gmail.com>
2016-04-14 11:24:53 +0200

    updated key value

--- manifests/regedit.pp
+++ manifests/regedit.pp
@@ -3,13 +3,13 @@ class windowsk::regedit {
 registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
   ensure => present,
   type   => dword,
-  data   => '00000000',
+  data   => '00000001',
 }
 
 registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
   ensure => present,
   type   => dword,
-  data   => '00000000',
+  data   => '00000001',
 }
 
 }
